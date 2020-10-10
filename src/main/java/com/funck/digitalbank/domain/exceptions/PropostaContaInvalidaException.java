5da5fe2bc8560533ac2736dcbaea8ccf71e27d9b
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2020-10-09 21:24:37 -0300

    Corrigindo status code para entidade não processadas

--- src/main/java/com/funck/digitalbank/domain/exceptions/PropostaContaInvalidaException.java
+++ src/main/java/com/funck/digitalbank/domain/exceptions/PropostaContaInvalidaException.java
@@ -1,5 +1,9 @@
 package com.funck.digitalbank.domain.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class PropostaContaInvalidaException extends RuntimeException {
 
     public PropostaContaInvalidaException() {

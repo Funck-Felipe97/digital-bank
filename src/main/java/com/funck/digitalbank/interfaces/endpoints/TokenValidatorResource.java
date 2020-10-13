0b805943c75f9aa5e50bbac84028cd8b7bd26cce
Felipe Carvalho Funck <funck.felipe@hotmail.com>
2020-10-13 20:09:24 -0300

    Alterando endpoint de validação do token

--- src/main/java/com/funck/digitalbank/interfaces/endpoints/TokenValidatorResource.java
+++ src/main/java/com/funck/digitalbank/interfaces/endpoints/TokenValidatorResource.java
@@ -2,26 +2,20 @@ package com.funck.digitalbank.interfaces.endpoints;
 
 import com.funck.digitalbank.application.TokenValidator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
-@RequestMapping("/token")
 @RestController
 public class TokenValidatorResource implements AbstractResource {
 
     private final TokenValidator tokenValidator;
 
-    @GetMapping("/{contaId}")
-    public void validarToken(
-            @PathVariable final String contaId,
-            @RequestParam final String token,
-            final HttpServletResponse response) {
+    @PostMapping("/conta/{contaId}/token/{token}")
+    public void validarToken(@PathVariable final String contaId, @PathVariable final String token, final HttpServletResponse response) {
         tokenValidator.validar(contaId, token);
 
         response.setHeader("Location", createURI("conta/{id}/senha", contaId).toASCIIString());
